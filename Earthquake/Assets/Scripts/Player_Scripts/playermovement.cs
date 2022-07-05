869c0f956150411512d868690ea20da39a78c088
littledominic071 <64623568+littledominic071@users.noreply.github.com>
2022-07-05 18:26:44 -0400

    Run(partial)

--- Earthquake/Assets/Scripts/Player_Scripts/playermovement.cs
+++ Earthquake/Assets/Scripts/Player_Scripts/playermovement.cs
@@ -6,6 +6,8 @@ public class playermovement : MonoBehaviour
 {
 
     public float moveSpeed = 0.5f;
+    public float runmultiplier = 3f;
+    public float dummyy = 1f;
 
     public Rigidbody2D rb;
     public Animator animator;
@@ -17,7 +19,7 @@ public class playermovement : MonoBehaviour
     {
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
-
+        
        if (movement != Vector2.zero)
     {
          animator.SetFloat("Horizontal", movement.x);
@@ -31,7 +33,15 @@ public class playermovement : MonoBehaviour
 
     void FixedUpdate() 
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime); 
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            dummyy = runmultiplier;
+        }
+        else
+        {
+            dummyy = 1f;
+        }
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime * dummyy);
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime  * dummyy); 
     }
 }
